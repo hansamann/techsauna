43249dee933e11549f8d717067d02224665f03d6
Sven Haiges <sven.haiges@sap.com>
2022-12-18 16:51:58 +0100

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,18 @@
 </script>
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>
-      Munich Tech Sauna
-    </title>
-    <meta name="description" content="Munich Tech Sauna" />
-    <meta name="keywords" content="tech, sauna, munich" />
-    
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="manifest" href="/site.webmanifest" />
+	<title> Munich Tech Sauna </title>
+	<meta name="description" content="Munich Tech Sauna" />
+	<meta name="keywords" content="tech, sauna, munich" />
 
-  </head>
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
+</head>
 
 <slot />
