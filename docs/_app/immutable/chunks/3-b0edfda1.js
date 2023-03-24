cced98d3712210dc4393ea90be6362a666a73097
Sven Haiges <sven.haiges@sap.com>
2023-03-24 21:06:27 +0100

    update discord join

---
+++ docs/_app/immutable/chunks/3-b0edfda1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/imprint/_page.svelte-12304f45.js";export{t as component};
--- docs/_app/immutable/chunks/3-c4c0ff64.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/imprint/_page.svelte-8655439b.js";export{t as component};
