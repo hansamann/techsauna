d72d6c3f47a07e0f553c8d7454a53d74d76d23d6
Sven Haiges <sven.haiges@sap.com>
2023-04-21 06:06:01 +0200

    update for may 18

---
+++ docs/_app/immutable/chunks/2-5313a6c1.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-3a898d40.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
 	import NavLinks from '/src/components/NavLinks.svelte';
 
 	const inviteMastodon = 'https://muenchen.social/invite/Ygp4Uheb';
-	const datetime = 'Apr 20, 2023 - 17:30';
-	const location = 'Cosimawellenbad';
-	const locationLink = 'https://www.swm.de/baeder/schwimmen-sauna/cosimawellenbad';
+	const datetime = 'May 18, 2023 - 14:30';
+	const location = 'Therme Erding';
+	const locationLink = 'https://www.therme-erding.de/therme-sauna/vitaltherme-saunen/';
 </script>
 
 <body
